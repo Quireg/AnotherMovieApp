a3a23e06bc98f3196fd2c9223813c77f9c255063
Artur Menchenko <artur.menchenko@gmail.com>
2017-07-21 18:44:53 +0300

    garbage out

--- app/src/main/java/ua/in/quireg/anothermovieapp/activities/MovieActivity.java
+++ app/src/main/java/ua/in/quireg/anothermovieapp/activities/MovieActivity.java
@@ -1,36 +1,29 @@
 package ua.in.quireg.anothermovieapp.activities;
 
-import android.app.FragmentTransaction;
 import android.content.ContentValues;
-import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.ImageView;
 
+import com.squareup.picasso.Picasso;
+
 import ua.in.quireg.anothermovieapp.R;
-import ua.in.quireg.anothermovieapp.async.ImageFetcher;
 import ua.in.quireg.anothermovieapp.common.Constants;
 import ua.in.quireg.anothermovieapp.common.GeneralUtils;
-import ua.in.quireg.anothermovieapp.common.UriHelper;
 import ua.in.quireg.anothermovieapp.common.MovieItem;
-import ua.in.quireg.anothermovieapp.interfaces.FetchImageCallback;
+import ua.in.quireg.anothermovieapp.common.UriHelper;
 import ua.in.quireg.anothermovieapp.interfaces.OnFragmentInteractionListener;
 import ua.in.quireg.anothermovieapp.managers.MovieDatabaseContract;
 import ua.in.quireg.anothermovieapp.services.SyncReviewsService;
 import ua.in.quireg.anothermovieapp.ui.MovieDetailsActivityFragment;
 import ua.in.quireg.anothermovieapp.ui.MovieReviewFragment;
 
-public class MovieActivity extends AppCompatActivity implements FetchImageCallback, OnFragmentInteractionListener{
-
-    private CollapsingToolbarLayout collapsingToolbarLayout;
-    private ImageView collapsingToolbarImageView;
+public class MovieActivity extends AppCompatActivity implements OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,19 +36,19 @@ public class MovieActivity extends AppCompatActivity implements FetchImageCallba
         MovieItem movie = (MovieItem) getIntent().getExtras().getSerializable(Constants.MOVIE);
 
 
-        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
         collapsingToolbarLayout.setTitle(movie.getTitle());
         AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.appbar);
         appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
             @Override
             public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
-                //some other code here
-                ViewCompat.setElevation(appBarLayout, GeneralUtils.dipToPixels(getApplicationContext(), 6));
+               ViewCompat.setElevation(appBarLayout, GeneralUtils.dipToPixels(getApplicationContext(), 6));
             }
         });
-        collapsingToolbarImageView = (ImageView) findViewById(R.id.toolbar_imageview);
 
-        new ImageFetcher(this, getApplicationContext()).execute(UriHelper.getImageUri(movie.getBackdropPath(), Constants.IMAGE_SIZE_ORIGINAL));
+        ImageView collapsingToolbarImageView = (ImageView) findViewById(R.id.toolbar_imageview);
+        Uri imageUri = UriHelper.getImageUri(movie.getBackdropPath(), Constants.IMAGE_SIZE_ORIGINAL);
+        Picasso.with(getApplicationContext()).load(imageUri).into(collapsingToolbarImageView);
 
         MovieDetailsActivityFragment movieDetailsActivityFragment = new MovieDetailsActivityFragment();
         movieDetailsActivityFragment.setArguments(getIntent().getExtras());
@@ -65,16 +58,9 @@ public class MovieActivity extends AppCompatActivity implements FetchImageCallba
     }
 
     @Override
-    public void setImage(Bitmap bitmap) {
-        if (collapsingToolbarLayout != null && collapsingToolbarImageView != null) {
-            collapsingToolbarImageView.setImageBitmap(bitmap);
-        }
-    }
-
-    @Override
     public void onFragmentMessage(String tag, Object data) {
         MovieItem item = (MovieItem) data;
-        switch (tag){
+        switch (tag) {
             case Constants.ADD_TO_FAVOURITES:
                 ContentValues contentValues = new ContentValues();
                 contentValues.put(MovieDatabaseContract.FavouriteMovies._ID, item.getId());
--- app/src/main/java/ua/in/quireg/anothermovieapp/interfaces/FetchImageCallback.java
+++
@@ -1,8 +0,0 @@
-package ua.in.quireg.anothermovieapp.interfaces;
-
-
-import android.graphics.Bitmap;
-
-public interface FetchImageCallback {
-    public void setImage(Bitmap bitmap);
-}
